20538bea1c3e602f3f3c67484648593f5f336fb2
Ryan Hanna <hanna.ryan@gmail.com>
2016-01-05 16:16:52 +0000

    initial commit

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,31 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/jaeger.Html5Video/www/Html5Video.js",
+        "id": "jaeger.Html5Video.Html5Video",
+        "clobbers": [
+            "plugins.html5Video"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "jaeger.Html5Video": "1.2.1",
+    "cordova-plugin-device": "1.1.1-dev"
+}
+// BOTTOM OF METADATA
+});
